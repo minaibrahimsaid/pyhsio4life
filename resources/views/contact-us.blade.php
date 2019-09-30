cf0e3629aa4490ac46d095b1fbce214017597cf3
minaibrahimsaid <minaibrahimsaid1991@gmail.com>
2019-09-30 19:43:17 -0400

    make css generaic

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('view')
     <section class="mbr-section" id="msg-box5-n"
-        style=" padding-top: 0rem; padding-bottom: 25px; padding-top: 10rem; color:white">
+        style=" padding-top: 0rem; padding-bottom: 25px; padding-top: 10rem; color:white; background-image:  url(assets/images/bg.jpg);">
 
 
         <div class="container">
