b26c3cb9f39b6a0a495eb1193081c27f13db26a6
minaibrahimsaid <minaibrahimsaid1991@gmail.com>
2019-10-13 19:21:57 -0400

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('view')
 <section class="mbr-slider mbr-section mbr-section__container carousel slide mbr-section-nopadding mbr-after-navbar"
-        data-ride="carousel" data-keyboard="false" data-wrap="true" data-pause="false" data-interval="500000"
+        data-ride="carousel" data-keyboard="false" data-wrap="true" data-pause="false" data-interval="5000"
         id="slider-0">
         <div>
             <div>
@@ -99,7 +99,7 @@
                                 <div class="mbr-overlay"></div>
                                 <div class="container-slide">
                                     <div class="row">
-                                        <div class="col-md-3" style="text-align:left; float: right;">
+                                        <div class="col-md-3" style="text-align:left; float: right;  margin-top: 50px;">
                                             <h1 class="mbr-section-title display-1 shadow-text" style="   
                                               font-family: Monotype Corsiva Regular; max-width: 250px;">Open 6 days a
                                                 week Saturday till
